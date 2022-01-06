36130bf87c67bf406e7264083656b3e5b0d4f235
jpwf <joaopedroweydt@gmail.com>
2022-01-06 00:03:51 -0300

    Estrutura feita

---
+++ public/scripts/index.js
@@ -0,0 +1,9 @@
+const sendresults = document.getElementById("send-info")
+const backhome = document.getElementById("close")
+
+sendresults.addEventListener("click", () => {
+  modal.classList.remove("hide")
+})
+backhome.addEventListener("click", () => {
+  modal.classList.add("hide")
+})
