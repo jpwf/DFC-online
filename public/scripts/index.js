2a4bb50d9cd9b3d7ed7b99c4615deb33e3d3311a
jpwf <joaopedroweydt@gmail.com>
2022-02-15 15:07:58 -0300

    Ideia no arquivo modal.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,24 +9,15 @@ const varncg = document.getElementById("VAR-NCG")
 const depreciation = document.getElementById("depreciation")
 const fcfirma = document.getElementById("fc-firma")
 
-
+// var ropliqvalue = ropliq.value
 
 sendresults.addEventListener("click", () => {
   if(debt.value !=0 & stocksquantity.value !=0 & ropliq.value !=0 & nopat.value != 0 & capex.value != 0 & varncg.value != 0 & depreciation.value != 0 & fcfirma.value != 0){
     window.location = "/fcd-results"
-    //var margem_nopat = parseInt((nopat.value/ropliq.value)/100)
    
-    
-    //
-    //NOPAT
-    // const nopat_1 = nopat.value * (1 + margem_nopat)
-    // const nopat_2 = nopat_1.value * (1 + margem_nopat)
-    // const nopat_3 = nopat_2.value * (1 + margem_nopat)
-    // const nopat_Perpet = nopat_3.value * (1 + margem_nopat)
   }else{
     alert("Preencha todos os campos")
     setTimeout(() =>{
-      // window.reload()
       debt.value = ""
       stocksquantity.value = ""
       ropliq.value = "" 
@@ -44,12 +35,13 @@ sendresults.addEventListener("click", () => {
 
 
 //métricas
-const perpetuidade = 0.01;
-const desconto_buffet = 0.1;
+// const perpetuidade = 0.01;
+// const desconto_buffet = 0.1;
 
-const desconto_taxa_juros = 0;
+// const desconto_taxa_juros = 0;
 //
 //Receita operacional Líquida
 
 //
 
+export { ropliq  };
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,21 +1,41 @@
-const backhome = document.getElementById("close")
-var ropliq_result_2 = document.getElementById("R-OP-LIQ-2")
-var ropliq_result_3 = document.getElementById("R-OP-LIQ-3")
-var ropliq_result_4 = document.getElementById("R-OP-LIQ-4")
-var ropliq_1 = parseInt(ropliq.value * (1 + 0.1))
-var ropliq_2 = parseInt(ropliq_1 * (1 + 0.1))
-var ropliq_3 = parseInt(ropliq_2 * (1 + 0.1))
-var ropliq_perpet = parseInt(ropliq_3 * (1 + 0))
+
+import { ropliq as rplq }  from './index.js';
+const backhome = document.getElementById("home")
+let ropliq_result_2 = document.getElementById("R-OP-LIQ-2")
+let ropliq_result_3 = document.getElementById("R-OP-LIQ-3")
+let ropliq_result_4 = document.getElementById("R-OP-LIQ-4")
+
 
 backhome.addEventListener("click", () => {
   window.location = "/"
+  alert("it works")
 })
 
+//testar colocar a págica como modal novamente, e ao invés de só usar display:none na fcd-results.html
+//Usar na index também, para que uma apareça e a outra não
+
+
+function ropliqcalc() {
+  
+  let ropliq_1 = parseInt(rplq * (1 + 0.1))
+  let ropliq_2 = parseInt(ropliq_1 * (1 + 0.1))
+  let ropliq_3 = parseInt(ropliq_2 * (1 + 0.1))
+  let ropliq_perpet = parseInt(ropliq_3 * (1 + 0))
 
 
 
 
-ropliq_result_1.textContent = parseInt(ropliq_1)
-ropliq_result_2.textContent = parseInt(ropliq_2)
-ropliq_result_3.textContent = parseInt(ropliq_3)
-ropliq_result_4.textContent = parseInt(ropliq_perpet)
+
+  ropliq_result_1.innerHTML = parseInt(ropliq_1)
+  ropliq_result_2.innerHTML = parseInt(ropliq_2)
+  ropliq_result_3.innerHTML = parseInt(ropliq_3)
+  ropliq_result_4.innerHTML = parseInt(ropliq_perpet)
+}
+ropliqcalc()
+
+//tentar criar uma função no arquivo index.js para armazenar esses valores:
+  
+  //
+//Porque eles travam a função do botão(por algum motivo que eu não faço ideia)
+
+
