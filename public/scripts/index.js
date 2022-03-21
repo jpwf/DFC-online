6acee15c352c20322a1b6fe8af9a7d69cd474f05
jpwf <joaopedroweydt@gmail.com>
2022-03-21 02:08:46 -0300

    Logic is working, back to modal style, script working

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,5 @@
 const sendresults = document.getElementById("send-info")
-const backhome = document.getElementById("home")
+const close = document.getElementById("home")
 
 const debt = document.getElementById("debt")
 const stocksquantity = document.getElementById("stocks-quantity")
@@ -10,11 +10,22 @@ const varncg = document.getElementById("VAR-NCG")
 const depreciation = document.getElementById("depreciation")
 const fcfirma = document.getElementById("fc-firma")
 
-// var ropliqvalue = ropliq.value
+
 
 sendresults.addEventListener("click", () => {
   if(debt.value !=0 & stocksquantity.value !=0 & ropliq.value !=0 & nopat.value != 0 & capex.value != 0 & varncg.value != 0 & depreciation.value != 0 & fcfirma.value != 0){
     modal.classList.remove("hide")
+    setTimeout(() =>{
+      debt.value = ""
+      stocksquantity.value = ""
+      ropliq.value = "" 
+      nopat.value = "" 
+      capex.value = "" 
+      varncg.value = "" 
+      depreciation.value = "" 
+      fcfirma.value = ""  
+    }, 1000)
+    ropliqcalc()
    
   }else{
     alert("Preencha todos os campos")
@@ -30,10 +41,43 @@ sendresults.addEventListener("click", () => {
     }, 2000)
   }
 })
-
-backhome.addEventListener("click",() => {
+close.addEventListener("click", () =>{
   modal.classList.add("hide")
 })
+
+
+function ropliqcalc() {
+  let rplq = ropliq.value
+  let ropliq_1 = parseInt(rplq * (1 + 0.1))
+  let ropliq_2 = parseInt(ropliq_1 * (1 + 0.1))
+  let ropliq_3 = parseInt(ropliq_2 * (1 + 0.1))
+  let ropliq_perpet = parseInt(ropliq_3 * (1 + 0.1))
+
+  let ropliq_result_1 = document.getElementById("R-OP-LIQ-1")
+  let ropliq_result_2 = document.getElementById("R-OP-LIQ-2")
+  let ropliq_result_3 = document.getElementById("R-OP-LIQ-3")
+  let ropliq_result_4 = document.getElementById("R-OP-LIQ-4")
+
+
+
+  ropliq_result_1.innerHTML = parseInt(ropliq_1)
+  ropliq_result_2.innerHTML = parseInt(ropliq_2)
+  ropliq_result_3.innerHTML = parseInt(ropliq_3)
+  ropliq_result_4.innerHTML = parseInt(ropliq_perpet)
+}
+
+
+// var ropliqvalue = ropliq.value
+
+// const infos = {
+//     ropliqvalue,
+//     nopat,
+//     capex,
+//     varncg,
+//     depreciation,
+//     fcfirma,
+    
+// }
 
 
 
@@ -48,4 +92,4 @@ backhome.addEventListener("click",() => {
 
 //
 
-export { ropliq  };
+// export { infos };
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,37 +1,40 @@
 
-import { ropliq as rplq }  from './index.js';
-const backhome = document.getElementById("home")
-let ropliq_result_2 = document.getElementById("R-OP-LIQ-2")
-let ropliq_result_3 = document.getElementById("R-OP-LIQ-3")
-let ropliq_result_4 = document.getElementById("R-OP-LIQ-4")
+// import { infos as infos } from './index.js';
+// // const backhome = document.getElementById("home")
 
 
-backhome.addEventListener("click", () => {
-  window.location = "/"
-  alert("it works")
-})
+
+// backhome.addEventListener("click", () => {
+//   window.location = "/"
+//   alert("it works")
+// })
 
 //testar colocar a págica como modal novamente, e ao invés de só usar display:none na fcd-results.html
 //Usar na index também, para que uma apareça e a outra não
 
 
-function ropliqcalc() {
-  
-  let ropliq_1 = parseInt(rplq * (1 + 0.1))
-  let ropliq_2 = parseInt(ropliq_1 * (1 + 0.1))
-  let ropliq_3 = parseInt(ropliq_2 * (1 + 0.1))
-  let ropliq_perpet = parseInt(ropliq_3 * (1 + 0))
+// function ropliqcalc() {
+//   infos.ropliqvalue = rplq;
+//   let ropliq_1 = parseInt(rplq * (1 + 0.1))
+//   let ropliq_2 = parseInt(ropliq_1 * (1 + 0.1))
+//   let ropliq_3 = parseInt(ropliq_2 * (1 + 0.1))
+//   let ropliq_perpet = parseInt(ropliq_3 * (1 + 0))
 
 
 
 
 
-  ropliq_result_1.innerHTML = parseInt(ropliq_1)
-  ropliq_result_2.innerHTML = parseInt(ropliq_2)
-  ropliq_result_3.innerHTML = parseInt(ropliq_3)
-  ropliq_result_4.innerHTML = parseInt(ropliq_perpet)
-}
-ropliqcalc()
+//   // ropliq_result_1.innerHTML = parseInt(ropliq_1)
+//   // ropliq_result_2.innerHTML = parseInt(ropliq_2)
+//   // ropliq_result_3.innerHTML = parseInt(ropliq_3)
+//   // ropliq_result_4.innerHTML = parseInt(ropliq_perpet)
+
+//   console.log(ropliq_1)
+//   console.log(ropliq_2)
+//   console.log(ropliq_3)
+//   console.log(ropliq_perpet)
+// }
+// ropliqcalc()
 
 //tentar criar uma função no arquivo index.js para armazenar esses valores:
   
